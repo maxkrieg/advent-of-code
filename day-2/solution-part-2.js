55adf6e2cb5763c73d4cecd069f9d7c44e1d7b11
Max Krieg <maxwell.krieg@gmail.com>
2018-12-13 11:04:07 -0500

    Rename day 2 files, start day 2 part 2

---
+++ day-2/solution-part-2.js
@@ -0,0 +1,11 @@
+const fs = require('fs')
+
+function run(fileData) {
+  const boxIdsList = getBoxIdsList(fileData)
+}
+
+function getBoxIdsList(fileData) {
+  return data.split('\n')
+}
+
+run(fs.readFileSync('input.txt', 'utf-8'))
