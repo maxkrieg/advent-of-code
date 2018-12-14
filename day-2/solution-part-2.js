91d7bd9e05ec1403ab874c6b21e5c80b23487445
Max Krieg <maxwell.krieg@gmail.com>
2018-12-14 18:06:36 -0500

    Add a little more refactoring

--- day-2/solution-part-2.js
+++ day-2/solution-part-2.js
@@ -2,26 +2,31 @@ const fs = require('fs')
 
 function run(fileData) {
   const boxIdsList = getBoxIdsList(fileData)
-  const matches = []
-
+  const allIdMatches = []
   boxIdsList.forEach(currentId => {
-    const idMatches = getIdMatches(currentId, boxIdsList)
-    matches.push(...idMatches, currentId)
+    if (!allIdMatches.includes(currentId)) {
+      const currentIdMatches = getIdsOffByOne(currentId, boxIdsList)
+      if (currentIdMatches.length > 0) {
+        allIdMatches.push(...currentIdMatches, currentId)
+      }
+    }
   })
+  console.log(allIdMatches)
+  return allIdMatches
 }
 
-function getIdMatches(currentId, boxIdsList) {
+function getIdsOffByOne(currentId, boxIdsList) {
   const matches = []
-  boxIdsList.forEach(comparisonId => {
-    const idsDifferByOne = isIdMatch(currentId, comparisonId)
+  boxIdsList.forEach(id => {
+    const idsDifferByOne = isOffByOne(currentId, id)
     if (idsDifferByOne) {
-      matches.push(comparisonId)
+      matches.push(id)
     }
   })
   return matches
 }
 
-function isIdMatch(idOne, idTwo) {
+function isOffByOne(idOne, idTwo) {
   let characterDifferences = 0
   for (let i = 0; i < idOne.length; i++) {
     const isMatch = idOne.charAt(i) === idTwo.charAt(i)
@@ -36,7 +41,7 @@ function isIdMatch(idOne, idTwo) {
 }
 
 function getBoxIdsList(fileData) {
-  return data.split('\n')
+  return fileData.split('\n')
 }
 
 run(fs.readFileSync('input.txt', 'utf-8'))
