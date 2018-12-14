5e5bc0ea1413868ebbf59800f450a3a32cb8b4c9
Max Krieg <maxwell.krieg@gmail.com>
2018-12-14 13:02:01 -0500

    Refactor/progress day 2 part 2 solution

--- day-2/solution-part-2.js
+++ day-2/solution-part-2.js
@@ -2,6 +2,37 @@ const fs = require('fs')
 
 function run(fileData) {
   const boxIdsList = getBoxIdsList(fileData)
+  const matches = []
+
+  boxIdsList.forEach(currentId => {
+    const idMatches = getIdMatches(currentId, boxIdsList)
+    matches.push(...idMatches, currentId)
+  })
+}
+
+function getIdMatches(currentId, boxIdsList) {
+  const matches = []
+  boxIdsList.forEach(comparisonId => {
+    const idsDifferByOne = isIdMatch(currentId, comparisonId)
+    if (idsDifferByOne) {
+      matches.push(comparisonId)
+    }
+  })
+  return matches
+}
+
+function isIdMatch(idOne, idTwo) {
+  let characterDifferences = 0
+  for (let i = 0; i < idOne.length; i++) {
+    const isMatch = idOne.charAt(i) === idTwo.charAt(i)
+    if (!isMatch) {
+      characterDifferences++
+    }
+    if (characterDifferences > 1) {
+      return false
+    }
+  }
+  return true
 }
 
 function getBoxIdsList(fileData) {
